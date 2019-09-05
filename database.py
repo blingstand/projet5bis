485404851ac606df9549e48cbbfa21def14cfe57
blingstand <blingstand@hotmail.fr>
2019-09-05 16:12:28 +0200

    script insertion data in tab

--- database.py
+++ database.py
@@ -4,31 +4,29 @@ class DbConnector():
     """class that manage the connection to the database and the fonction related to db"""
 
     COLUMNS = 'name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags,\
-    minerals_tags, palm_oil, composition, link, quantity, brands, nutriments'
+    minerals_tags, palm_oil, link, quantity, brands, nutriments'
 
     def __init__(self):
         self.mydb = mysql.connector.connect(
             host="localhost",
             user="root",
             passwd="123",
-            database="python",
-            auth_plugin='mysql_native_password'
+            database="python"
         )
         self.my_cursor = self.mydb.cursor()
 
-    def add_substitute(self, name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, composition, link, quantity, brands, nutriments):
+    def add_substitute(self, name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, link, quantity, brands, nutriments):
         """
         Inserts a line in the table product
         """
         sql = 'INSERT INTO Product ({})'\
         'VALUES ("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}",'\
-        ' "{}", "{}");'.format(self.COLUMNS, name, labels, additives, packagings,nutrition_grade, nova_group, traces, \
-        manufacturing_places_tags, minerals_tags, palm_oil, composition, link, quantity, brands, nutriments)
-        input(sql)
+        ' "{}");'.format(self.COLUMNS, name, labels, additives, packagings, nutrition_grade, \
+        nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, link, quantity, brands, nutriments)
+
         try:
             self.my_cursor.execute(sql)
             self.mydb.commit() #has to commit the change
-            print("-- db good.")
         except Exception as e:
             raise e
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # autoformation à openfoodfacts
-
+# pour importer la lib openfoodfact : pip install git+https://github.com/openfoodfacts/openfoodfacts-python
 """
     Maintenant je dois placer ces données dans la base :
         -1 tester le remplissage de la table avec un set de valeurs
@@ -9,7 +9,7 @@
 import openfoodfacts
 import database as db
 
-class Category():
+class Product():
 
     TUP_CATEGORIES = openfoodfacts.facets.get_categories()
     SIZE_RESEARCH = 50
@@ -18,15 +18,14 @@ class Category():
     "Pizzas", "Tartes salées", "Spaghetti", "Riz", "Glaces", "Chocolat noir", "Soupes") #5+7+8
     TUP_CRIT = ("product_name_fr", "labels", "additives_original_tags", "packaging_tags", "nutrition_grades",\
     "nova_group", "traces","manufacturing_places_tags","minerals_tags",\
-    "ingredients_from_or_that_may_be_from_palm_oil_n","ingredients_text_with_allergens_fr"\
+    "ingredients_from_or_that_may_be_from_palm_oil_n"\
     ,"link", "product_quantity", "brands_tags", "nutriments")
 
     def __init__(self):
-        self.dict_prod= self.get_categories(self.TUP_CATEGORIES)
-        # self.products = self.get_products(self.categories)
-        # self.twenty_fr_categories_with_20_prod = self.get_categories(self.fr_categories, self.TUP_CATEGORIES)
+        self.dict_prod = self.get_categories(self.TUP_CATEGORIES)
 
-    def selected_crit_for_prod(self, product):
+
+    def _selected_crit_for_prod(self, product):
         """Takes a product and keep only crit concerning itself based on a tuple of criterions"""
 
         final_product = {}
@@ -37,16 +36,16 @@ class Category():
                 final_product[crit] = None
         return final_product
 
-    def fill_table(self):
-        """Fills the table of a given db
-            -1 connexion
-            -2 requête
+
+    def fill_table(self, name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, link, quantity, brands, nutriments):
+        """Fills the table of a given db from DbConnector
+            -1 connection
+            -2 request
         """
         try:
             db_conn = db.DbConnector()
-            input("connexion réussie ! =) tentative d'insertion")
-            db_conn.add_substitute("name", "labels", "additives", "packagings", "a", "a", "traces", "manufacturing_places_tags", "minerals_tags", "0", "composition", "link", "100g", "brands", "nutriments")
-            print("Succès")
+
+            db_conn.add_substitute(name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, link, quantity, brands, nutriments)
         except Exception as e:
             raise e
         pass
@@ -55,7 +54,7 @@ class Category():
         """Checks wether each element of the list contains at least 20 elements,
         then returns a liste of categories with 20 prod inside """
 
-        dico_cat = {} #create dico
+        dict_prod = {} #create dico
 
         print("Interroge l'API ! ")
         try :
@@ -80,16 +79,9 @@ class Category():
                     nom_prod = prod["product_name_fr"]
                     if nom_prod not in liste_nom_prod and len(liste_nom_prod) < 20: #anti-duplicate
                         liste_nom_prod.append(nom_prod)
-                        liste_final_prod.append(self.selected_crit_for_prod(prod))
-                dico_cat[category] = liste_final_prod
-            """
-            GERE AFFICHAGE DU DICO PR DEBUG
-            for cat in dico_cat:
-                for dictionnaire in dico_cat[cat]:
-                    if len(str(dictionnaire["ingredients_text_with_allergens_fr"])) > 1500:
-                        print("\t--({}) : ".format(len(str(dictionnaire["ingredients_text_with_allergens_fr"]))),dictionnaire["ingredients_text_with_allergens_fr"])
-            """
-            return dico_cat
+                        liste_final_prod.append(self._selected_crit_for_prod(prod))
+                dict_prod[category] = liste_final_prod
+            return dict_prod
         except Exception as e :
             raise e
 
@@ -100,32 +92,37 @@ class Category():
 
 
 def main():
-    # print("- "*30)
-    # input_answer = input("\nChoisissez une catégorie parmi la liste suivante : {}\n  >"\
-    #     .format(TUP_CATEGORIES))
-    # print("- "*30)
-    # print("\nVous avez choisi : {}".format(TUP_CATEGORIES[int(input_answer)]))
+    prod = Product()
+    ajout = 0
+    for category in prod.dict_prod:
+        for product in prod.dict_prod[category]:
+            name = product["product_name_fr"]
+            labels = product["labels"]
+            additives = product["additives_original_tags"]
+            packagings = product["packaging_tags"]
+            nutrition_grade = product["nutrition_grades"]
+            nova_group = product["nova_group"]
+            traces = product["traces"]
+            manufacturing_places_tags = product["manufacturing_places_tags"]
+            minerals_tags = product["minerals_tags"]
+            palm_oil = product["ingredients_from_or_that_may_be_from_palm_oil_n"]
+            link = product["link"]
+            quantity = product["product_quantity"]
+            brands = product["brands_tags"]
+            nutriments = product["nutriments"]
 
-    cat = Category()
-        # len_search = len(results)
-        # print(str(i+1), j, "longueur de la recherche : {}".format(len_search))
+            try:
 
+                prod.fill_table(name, labels, additives, packagings,nutrition_grade, nova_group, traces, manufacturing_places_tags, minerals_tags, palm_oil, link, quantity, brands, nutriments)
+                ajout += 1
+            except Exception as e:
+                ajout -= 1
+                print("- - "*50)
+                print(name, len(traces), traces)
+                print("- - "*50)
+                raise e
+
+    print("ligne écrites = {}".format(ajout))
 
 if __name__ == '__main__':
     main()
-"""
- {'product_name_fr': 'Velouté de potiron à la crème fraiche',
- 'additives_original_tags': [],
- 'packaging_tags': ['brique', 'carton'],
- 'nutrition_grades': 'c',
- 'nova_group': 4,
- 'traces': 'en:celery,en:eggs,en:gluten,en:mustard',
- 'manufacturing_places_tags': [],
- 'minerals_tags': [],
- 'ingredients_from_or_that_may_be_from_palm_oil_n': 0,
- 'ingredients_text_with_allergens_fr': 'Légumes 53% (potiron 29%, carotte 16%, oignon, pomme de terre, tomate), eau,<span class="allergen">crème</span> fraîche 1,6%,<span class="allergen">beurre</span>,  sel, sucre, extrait de levure, arômes (dont_<span class="allergen">lait</span>_)',
- 'link': '',
- 'product_quantity': 1000,
- 'brands_tags': ['knorr'],
- 'nutriments': {'sugars_value': '2.2', 'saturated-fat_serving': 3, 'sugars_100g': '2.2', 'nutrition-score-fr_100g': 3, 'sodium_value': '0.28', 'salt': '0.7', 'carbohydrates_value': '3.9', 'energy_100g': 159, 'nutrition-score-uk_serving': 3, 'carbohydrates': '3.9', 'fiber_100g': 1, 'sodium_100g': '0.28', 'energy': 159, 'carbohydrates_100g': '3.9', 'nutrition-score-uk': 3, 'carbon-footprint-from-known-ingredients_product': 622, 'saturated-fat_value': '1.2', 'nutrition-score-fr_serving': 3, 'fat': '1.9', 'sugars_unit': 'g', 'nova-group': 4, 'proteins_value': '0.7', 'salt_unit': 'g', 'energy_serving': 398, 'salt_100g': '0.7', 'fiber_serving': '2.5', 'fat_100g': '1.9', 'sugars': '2.2', 'proteins_serving': '1.75', 'sodium_serving': '0.7', 'saturated-fat': '1.2', 'carbohydrates_serving': '9.75', 'fat_unit': 'g', 'sodium': '0.28', 'nova-group_serving': 4, 'carbon-footprint-from-known-ingredients_100g': '62.22', 'nutrition-score-fr': 3, 'energy_unit': 'kcal', 'carbon-footprint-from-known-ingredients_serving': 156, 'fiber_value': 1, 'fat_serving': '4.75', 'carbohydrates_unit': 'g', 'saturated-fat_unit': 'g', 'saturated-fat_100g': '1.2', 'salt_serving': '1.75', 'fiber_unit': 'g', 'fiber': 1, 'nutrition-score-uk_100g': 3, 'energy_value': 38, 'proteins': '0.7', 'proteins_100g': '0.7', 'sugars_serving': '5.5', 'nova-group_100g': 4, 'sodium_unit': 'g', 'fat_value': '1.9', 'proteins_unit': 'g', 'salt_value': '0.7'}}]}
- """
